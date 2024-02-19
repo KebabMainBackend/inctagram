54cbdf4a3a7b302c57471594b7ea1fa7b4ed84e4
Sansrona <kingJAM00@yandex.ru>
2024-02-19 19:46:44 +0600

    fix: return proper error

--- apps/inctagram/src/auth/application/use-cases/verify-confirmation-code.command.ts
+++ apps/inctagram/src/auth/application/use-cases/verify-confirmation-code.command.ts
@@ -14,13 +14,17 @@ export class VerifyConfirmationCodeHandler
   constructor(private usersRepo: UsersRepository) {}
   async execute({ code }: VerifyConfirmationCodeCommand) {
     const user = await this.usersRepo.getUserByCode(code);
+
     if (!user) {
       const error = createErrorMessage('invalid code', 'code');
       throw new HttpException(error, HttpStatus.BAD_REQUEST);
     }
     if (user.confirmationData.codeExpirationDate < new Date()) {
-      const error = createErrorMessage('code expired', 'code');
-      throw new HttpException(error, HttpStatus.BAD_REQUEST);
+      const error = {
+        error: 'code expired',
+        email: user.email,
+      };
+      throw new HttpException(error, HttpStatus.FORBIDDEN);
     }
     if (user.isConfirmed) {
       const error = createErrorMessage('code already confirmed', 'code');
