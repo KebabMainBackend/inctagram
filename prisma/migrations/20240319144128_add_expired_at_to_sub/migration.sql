72fcbd91ed9e5b38779bba7a75c0820c4b9c25db
Sansrona <kingJAM00@yandex.ru>
2024-04-01 14:32:57 +0500

    feat: add migrations

--- prisma/migrations/20240319144128_add_expired_at_to_sub/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `expiredAt` to the `Subscription` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Subscription" ADD COLUMN     "expiredAt" TIMESTAMP(3) NOT NULL;
